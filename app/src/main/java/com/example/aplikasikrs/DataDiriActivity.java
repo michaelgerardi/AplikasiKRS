3d67fa3e5fe19986a1884104fb4344f530c55e37
mikelgerard <mikelgerard17@gmail.com>
2019-10-24 21:37:23 +0700

    first Commit

--- app/src/main/java/com/example/aplikasikrs/DataDiriActivity.java
+++ app/src/main/java/com/example/aplikasikrs/DataDiriActivity.java
@@ -2,7 +2,10 @@ package com.example.aplikasikrs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DataDiriActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class DataDiriActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_diri);
         this.setTitle("SI KRS - HAI Michael");
+
+        Button simpan1 = findViewById(R.id.button7);
+        simpan1.setOnClickListener(btspn1);
     }
+    private View.OnClickListener btspn1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(DataDiriActivity.this,DaftarIsiActivity.class);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/com/example/aplikasikrs/EditMatakuliahActivity.java
+++ app/src/main/java/com/example/aplikasikrs/EditMatakuliahActivity.java
@@ -9,9 +9,11 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 
 public class EditMatakuliahActivity extends AppCompatActivity {
+    private Spinner spinner1;
+    private Spinner spinner2;
     private  static  final String[] hari = {"senin","selala","rabu","kamis","jumat"};
     private  static  final String[]Sesi = {"1","2","3","4"};
-    private Spinner spinner1,spinner2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,6 @@ public class EditMatakuliahActivity extends AppCompatActivity {
         spinner2= findViewById(R.id.spinner2);
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(EditMatakuliahActivity.this,android.R.layout.simple_spinner_item,Sesi);
         adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        spinner1.setAdapter(adapter1);
+        spinner2.setAdapter(adapter1);
     }
 }
